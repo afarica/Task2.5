cc90ac06be4b7385af765202881217589fc7ec0b
afarica <57631090+afarica@users.noreply.github.com>
2019-11-15 00:44:28 +0600

    Update p25.py

--- p25.py
+++ p25.py
@@ -10,14 +10,7 @@
 a=int(input('A class:'))
 b=int(input('B class:'))
 c=int(input("C class:"))
-d=a//2
-d1=a%2 
-i=b//2
-i1=b%2
-f=c//2
-f1=c%2
-g=d1+i1+f1
-if g>=1:
-	print(d+i+f+g)
-else:
-	print(d+i+f)
+d=a/2
+i=b/2
+f=c/2
+print(round(d+i+f))
